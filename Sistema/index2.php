55d13caa0fa52dee0d722fb8ed8f889b0c768176
rodrigo730 <ro783073@gmail.com>
2024-10-08 20:56:59 -0300

    Atualização

--- Sistema/index2.php
+++ Sistema/index2.php
@@ -4,23 +4,23 @@ use phpseclib3\Net\SCP;
 
 require 'vendor/autoload.php';
 
-// Configurações de conexão SSH
+
 $servidor = 'seuservidor.com';
 $usuario = 'seu_usuario';
 $senha = 'sua_senha';
 $pastaLocal = '/caminho/para/pasta_local';
 $pastaRemota = '/caminho/para/pasta_remota';
 
-// Conectar via SSH
+
 $ssh = new SSH2($servidor);
 if (!$ssh->login($usuario, $senha)) {
     die('Falha ao fazer login no servidor SSH.');
 }
 
-// Criar uma instância do SCP
+
 $scp = new SCP($ssh);
 
-// Função para enviar arquivos recursivamente
+
 function enviarPastaViaSCP($scp, $pastaLocal, $pastaRemota) {
     foreach (scandir($pastaLocal) as $file) {
         if ($file != "." && $file != "..") {
@@ -28,11 +28,11 @@ function enviarPastaViaSCP($scp, $pastaLocal, $pastaRemota) {
             $caminhoRemoto = $pastaRemota . '/' . $file;
 
             if (is_dir($caminhoLocal)) {
-                // Cria a pasta no servidor e envia recursivamente
+                
                 $scp->exec("mkdir -p " . escapeshellarg($caminhoRemoto));
                 enviarPastaViaSCP($scp, $caminhoLocal, $caminhoRemoto);
             } else {
-                // Envia o arquivo
+                
                 $scp->put($caminhoRemoto, $caminhoLocal, SCP::SOURCE_LOCAL_FILE);
                 echo "Arquivo enviado: $file\n";
             }
@@ -40,6 +40,6 @@ function enviarPastaViaSCP($scp, $pastaLocal, $pastaRemota) {
     }
 }
 
-// Enviar arquivos via SCP
+
 enviarPastaViaSCP($scp, $pastaLocal, $pastaRemota);
 ?>
